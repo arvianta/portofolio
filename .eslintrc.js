1c13a60597afd8505656511d5fb323a3a8db6014
Rayhan Arvianta Bayuputra <rayhan.arvianta@gmail.com>
2024-06-23 19:15:23 +0700

    fix: eslintrc prettier duplicate key

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,6 @@ module.exports = {
     "react/jsx-indent-props": "off",
     "react/jsx-indent": "off",
     "react/prop-types": "off",
-    "prettier/prettier": "off",
     "react/no-array-index-key": "off",
     "react/jsx-props-no-spreading": "off",
     "react/jsx-one-expression-per-line": "off",
